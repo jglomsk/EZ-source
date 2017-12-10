e04824d0289d554b00d6ab002b4d05b70e21d8c0
Jacob Glomski <jglomsk@Jacobs-MacBook-Air.local>
2017-12-10 11:59:01 -0500

    Oopsies on the mac version

--- upToLoc/source/ez.hpp
+++ upToLoc/source/ez.hpp
@@ -508,7 +508,7 @@ void Main::translate(const char* file, // the file itself
 						fp_copy = std::string("\"C:\\Program Files\\EZ\\") + fp_add + std::string("\"");
 					}
 					else if (PLATFORM == 2) {
-						fp_copy = std::string("/usr/share/ez/") + fp_add;
+						fp_copy = std::string("/usr/local/share/ez/") + fp_add;
 					}
 					std::ifstream fp_test(fp_copy.c_str());
 					if (fp_test) translate(fp_copy.c_str(), variables, values, baskets, count);
--- uploadtoEZ-lang/eznix_install.sh
+++ uploadtoEZ-lang/eznix_install.sh
@@ -1,11 +1,15 @@
-sudo chmod +x ez.out             # change perms
-sudo cp ez.out /usr/bin/ez       # create a copy of the ez interpreter in the path
-sudo mkdir /usr/share/ez/        # make our own dir
-sudo cp help.ez /usr/share/ez    # create a copy of help.ez in our desired location
-sudo cp creator.ez /usr/share/ez
-sudo cp sauce.ez /usr/share/ez
-sudo cp all.ez /usr/share/ez
-sudo rm -rf help.ez              # then delete that file
+sudo chmod +x ez.out # change perms
+sudo chmod +x ez.mac
+sudo mkdir /usr/local/bin
+sudo mkdir /usr/local/share
+sudo mkdir /usr/local/share/ez
+sudo cp ez.out /usr/bin/ez # create a copy of the ez interpreter in the path
+sudo cp ez.mac /usr/local/bin/ez
+sudo cp help.ez /usr/local/share/ez/ # create copy of libs
+sudo cp creator.ez /usr/local/share/ez/
+sudo cp sauce.ez /usr/local/share/ez/
+sudo cp all.ez /usr/local/share/ez/
+sudo rm -rf help.ez # then delete that file
 sudo rm -rf creator.ez
 sudo rm -rf sauce.ez
 sudo rm -rf all.ez
@@ -13,4 +17,4 @@ echo "Command: ez"
 echo "Usage: ez <ez-file>\n"
 echo "Description: "
 echo "Uses the ez interpreter to run a .ez file.\n"
-echo "Install completed successfully." # all done
+echo "Install completed successfully."
