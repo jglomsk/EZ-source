214a72e1af1d04e7a075ff67894fa3a0b5c3fa91
coderhelpfromtwitter <jglomski316@gmail.com>
2017-12-29 11:20:47 -0500

    Fixed deletion of files when not sudo

--- source/install.cpp
+++ source/install.cpp
@@ -2,13 +2,9 @@
 int main()
 {
   system("mkdir \"C:\\Program Files\\EZ\"");
-  system("copy \"help.ez\" \"C:\\Program Files\\EZ\"");
-  system("copy \"creator.ez\" \"C:\\Program Files\\EZ\"");
-  system("copy \"sauce.ez\" \"C:\\Program Files\\EZ\"");
-  system("copy \"all.ez\" \"C:\\Program Files\\EZ\"");
-  system("del \"help.ez\"");
-  system("del \"creator.ez\"");
-  system("del \"sauce.ez\"");
-  system("del \"all.ez\"");
+  system("copy \"help.ez\" \"C:\\Program Files\\EZ\" && del \"help.ez\"");
+  system("copy \"creator.ez\" \"C:\\Program Files\\EZ\" && del \"creator.ez\"");
+  system("copy \"sauce.ez\" \"C:\\Program Files\\EZ\" && del \"sauce.ez\"");
+  system("copy \"all.ez\" \"C:\\Program Files\\EZ\" && del \"all.ez\"");
   return 0;
 }
--- uploadtoEZ-lang/eznix_install.bash
+++ uploadtoEZ-lang/eznix_install.bash
@@ -1,8 +1,8 @@
 sudo chmod +x ez.out # change perms
 sudo chmod +x ez.mac
-sudo mkdir /usr/local/bin
-sudo mkdir /usr/local/share
-sudo mkdir /usr/local/share/ez
+sudo mkdir /usr/local/bin &>/dev/null
+sudo mkdir /usr/local/share &>/dev/null
+sudo mkdir /usr/local/share/ez &>/dev/null
 if [ "$(uname)" == "Linux" ]; then
 	sudo cp ez.out /usr/local/bin/ez
 	sudo rm -rf ez.mac
@@ -10,14 +10,10 @@ else
 	sudo cp ez.mac /usr/local/bin/ez
 	sudo rm -rf ez.out
 fi
-sudo cp help.ez /usr/local/share/ez/ # create copy of libs
-sudo cp creator.ez /usr/local/share/ez/
-sudo cp sauce.ez /usr/local/share/ez/
-sudo cp all.ez /usr/local/share/ez/
-sudo rm -rf help.ez # then delete that file
-sudo rm -rf creator.ez
-sudo rm -rf sauce.ez
-sudo rm -rf all.ez
+sudo cp help.ez /usr/local/share/ez/ && sudo rm -rf help.ez # copy libs then delete that file
+sudo cp creator.ez /usr/local/share/ez/ && sudo rm -rf creator.ez
+sudo cp sauce.ez /usr/local/share/ez/ && sudo rm -rf sauce.ez
+sudo cp all.ez /usr/local/share/ez/ && sudo rm -rf all.ez
 sudo rm -rf ezwin_install.exe
 sudo rm -rf ez.exe
 read -n1 -p "Would you like to delete the original interpreter? [y,n]: " y_or_n;
